6791753c300a53dfb5a040791cfbf8f073105738
brunokiedis <brunolimadevelopment@gmail.com>
2021-09-17 16:53:48 -0300

    Start app gameplay

---
+++ src/components/ListDivider/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+import { View } from 'react-native';
+import { styles } from './styles';
+
+export function ListDivider() {
+    return (
+        <View style={styles.container}/>
+    );
+    
+}
